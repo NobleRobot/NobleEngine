f544e221b619f059a39a9b6c91527825f988fd99
Mark LaCroix <mark@noblerobot.com>
2023-03-30 00:37:05 -0500

    Added configuration API to Noble.lua to address this PR.

--- utilities/Utilities.lua
+++ utilities/Utilities.lua
@@ -7,6 +7,16 @@
 
 Utilities = {}
 
+function Utilities.copy(obj, seen)
+	if type(obj) ~= 'table' then return obj end
+	if seen and seen[obj] then return seen[obj] end
+	local s = seen or {}
+	local res = setmetatable({}, getmetatable(obj))
+	s[obj] = res
+	for k, v in pairs(obj) do res[copy(k, s)] = copy(v, s) end
+	return res
+end
+
 function Utilities.isNull(__object)
 	return __object == nil or __object == ''
 end
@@ -23,9 +33,9 @@ function Utilities.endsWith(__string, __ending)
 end
 
 function Utilities.handleOptionalBoolean(__argument, __default)
-	if __argument == nil then
+	if (__argument == nil) then
 		return __default
-	elseif __argument == true or __argument == false then
+	elseif (__argument == true or __argument == false) then
 		return __argument
 	else
 		error("BONK: You shouldn’t pass non-boolean value as a boolean parameter.")
@@ -57,9 +67,9 @@ function Utilities.screenshot()
 end
 
 function Utilities.autoTable(__numberOfDimensions)
-    local metatable = {};
-    for i = 1, __numberOfDimensions do
-        metatable[i] = {
+	local metatable = {};
+	for i = 1, __numberOfDimensions do
+		metatable[i] = {
 			__index = function(__table, __key)
 				if i < __numberOfDimensions then
 					__table[__key] = setmetatable({}, metatable[i+1])
@@ -67,8 +77,8 @@ function Utilities.autoTable(__numberOfDimensions)
 				end
 			end
 		}
-    end
-    return setmetatable({}, metatable[1]);
+	end
+	return setmetatable({}, metatable[1]);
 end
 
 function Utilities.newUUID()
@@ -85,49 +95,49 @@ end
 --
 
 function math.clamp(__value, __min, __max)
-    if (__min > __max) then
-        __min, __max = __max, __min
-    end
-    return math.max(__min, math.min(__max, __value))
+	if (__min > __max) then
+		__min, __max = __max, __min
+	end
+	return math.max(__min, math.min(__max, __value))
 end
 
 function math.ring(__value, __min, __max)
-    if (__min > __max) then
-        __min, __max = __max, __min
-    end
-    return __min + (__value - __min) % (__max - __min)
+	if (__min > __max) then
+		__min, __max = __max, __min
+	end
+	return __min + (__value - __min) % (__max - __min)
 end
 
 function math.ringInt(__value, __min, __max)
-    return math.ring(__value, __min, __max + 1)
+	return math.ring(__value, __min, __max + 1)
 end
 
 function math.approach(_value, __target, __step)
-    if (_value == __target) then
-        return _value, true
-    end
-    local d = __target - _value
-    if (d > 0) then
-        _value = _value + __step
-        if (_value >= __target) then
-            return __target, true
-        else
-            return _value, false
-        end
-    elseif (d < 0) then
-        _value = _value - __step
-        if (_value <= __target) then
-            return __target, true
-        else
-            return _value, false
-        end
-    else
-        return _value, true
-    end
+	if (_value == __target) then
+		return _value, true
+	end
+	local d = __target - _value
+	if (d > 0) then
+		_value = _value + __step
+		if (_value >= __target) then
+			return __target, true
+		else
+			return _value, false
+		end
+	elseif (d < 0) then
+		_value = _value - __step
+		if (_value <= __target) then
+			return __target, true
+		else
+			return _value, false
+		end
+	else
+		return _value, true
+	end
 end
 
 function math.infiniteApproach(at_zero, at_infinite, x_halfway, x)
-    return at_infinite - (at_infinite - at_zero) * 0.5 ^ (x / x_halfway)
+	return at_infinite - (at_infinite - at_zero) * 0.5 ^ (x / x_halfway)
 end
 
 function math.round(__value, __bracket)
@@ -144,8 +154,8 @@ end
 --
 
 function table.random(__table)
-    if (type(__table) ~= "table") then return nil end
-    return __table[math.ceil(math.random(#__table))]
+	if (type(__table) ~= "table") then return nil end
+	return __table[math.ceil(math.random(#__table))]
 end
 function table.each(__table, __function)
 	if (type(__function) ~= "function") then return end
@@ -157,7 +167,7 @@ end
 function table.filter(__table, __filter)
 	local out = {}
 	for _, value in pairs(__table) do
-	  --if filterIter(v, k, table) then out[k] = v end
+	--if filterIter(v, k, table) then out[k] = v end
 		if (__filter(value)) then
 			__table.insert (out,value)
 		end
