7d926682e38b9ba9198f33c8d2046aa46950aa18
Mark LaCroix <mark@noblerobot.com>
2023-11-03 13:01:06 -0500

    Ease: Updated documentation and added "Ease.reverse" method.

--- utilities/Ease.lua
+++ utilities/Ease.lua
@@ -1,4 +1,5 @@
 --- Extensions to `playdate.easingFunctions`, aliased as `Ease` in Noble Engine.
+-- See the Playdate SDK for a list of easing functions.
 -- @module Ease
 
 local componentFunctions = {
@@ -12,37 +13,69 @@ local componentFunctions = {
 	[Ease.inOutElastic]	= {		enter = Ease.inElastic,		exit = Ease.outElastic	},
 	[Ease.inOutBack]	= {		enter = Ease.inBack,		exit = Ease.outBack		},
 	[Ease.inOutBounce]	= {		enter = Ease.inBounce,		exit = Ease.outBounce	},
-	[Ease.outInQuad]	= { 	enter = Ease.outQuad,		exit = Ease.inQuad		},
-	[Ease.outInCubic]	= { 	enter = Ease.outCubic,		exit = Ease.inCubic		},
-	[Ease.outInQuart]	= { 	enter = Ease.outQuart,		exit = Ease.inQuart		},
-	[Ease.outInQuint]	= { 	enter = Ease.outQuint,		exit = Ease.inQuint		},
-	[Ease.outInSine]	= { 	enter = Ease.outSine,		exit = Ease.inSine		},
-	[Ease.outInExpo]	= { 	enter = Ease.outExpo,		exit = Ease.inExpo		},
-	[Ease.outInCirc]	= { 	enter = Ease.outCirc,		exit = Ease.inCirc		},
-	[Ease.outInElastic]	= { 	enter = Ease.outElastic,	exit = Ease.inElastic	},
-	[Ease.outInBack]	= { 	enter = Ease.outBack,		exit = Ease.inBack		},
-	[Ease.outInBounce]	= { 	enter = Ease.outBounce,		exit = Ease.inBounce	},
+	[Ease.outInQuad]	= {		enter = Ease.outQuad,		exit = Ease.inQuad		},
+	[Ease.outInCubic]	= {		enter = Ease.outCubic,		exit = Ease.inCubic		},
+	[Ease.outInQuart]	= {		enter = Ease.outQuart,		exit = Ease.inQuart		},
+	[Ease.outInQuint]	= {		enter = Ease.outQuint,		exit = Ease.inQuint		},
+	[Ease.outInSine]	= {		enter = Ease.outSine,		exit = Ease.inSine		},
+	[Ease.outInExpo]	= {		enter = Ease.outExpo,		exit = Ease.inExpo		},
+	[Ease.outInCirc]	= {		enter = Ease.outCirc,		exit = Ease.inCirc		},
+	[Ease.outInElastic]	= {		enter = Ease.outElastic,	exit = Ease.inElastic	},
+	[Ease.outInBack]	= {		enter = Ease.outBack,		exit = Ease.inBack		},
+	[Ease.outInBounce]	= {		enter = Ease.outBounce,		exit = Ease.inBounce	},
 	[Ease.linear]		= {		enter = Ease.linear,		exit = Ease.linear		}
 }
 
---- Component Functions
---- Use these methods to quickly retrieve one of the two "halves" of an "inOut" or "outIn" easing function.
---- Returns `nil` for any easing function that isn't in the form of `Ease.inOutXxxx` or `Ease.outInXxxx`. `Ease.linear` returns itself. See the Playdate SDK for a list of easing functions.
---- Useful when you don't know until runtime which ease is going to be used, or when splitting up an ease across multiple animations (such as in Noble.Transition).
+local reverseFunctions = {
+	[Ease.inQuad]		= Ease.outQuad,
+	[Ease.inCubic]		= Ease.outCubic,
+	[Ease.inQuart]		= Ease.outQuart,
+	[Ease.inQuint]		= Ease.outQuint,
+	[Ease.inSine]		= Ease.outSine,
+	[Ease.inExpo]		= Ease.outExpo,
+	[Ease.inCirc]		= Ease.outCirc,
+	[Ease.inElastic]	= Ease.outElastic,
+	[Ease.inBack]		= Ease.outBack,
+	[Ease.inBounce]		= Ease.outBounce,
+	[Ease.outQuad]		= Ease.inQuad,
+	[Ease.outCubic]		= Ease.inCubic,
+	[Ease.outQuart]		= Ease.inQuart,
+	[Ease.outQuint]		= Ease.inQuint,
+	[Ease.outSine]		= Ease.inSine,
+	[Ease.outExpo]		= Ease.inExpo,
+	[Ease.outCirc]		= Ease.inCirc,
+	[Ease.outElastic]	= Ease.inElastic,
+	[Ease.outBack]		= Ease.inBack,
+	[Ease.outBounce]	= Ease.inBounce,
+	[Ease.linear]		= Ease.linear
+}
+
+--- Returns the first half of an "inOut" or "outIn" easing function.
+-- Returns `nil` for any easing function that isn't in the form of `Ease.inOutXxxx` or `Ease.outInXxxx`. `Ease.linear` returns itself.
 -- @usage
 -- local ease = Ease.outInQuad
 -- local easeEnter = Ease.enter(ease)	-- Returns "Ease.outQuad"
--- local easeExit = Ease.exit(ease)		-- Returns "Ease.inQuad"
--- @section componentFunctions
-
----
 function Ease.enter(__easingFunction)
 	if (componentFunctions[__easingFunction] == nil) then return nil end
 	return componentFunctions[__easingFunction].enter
 end
 
----
+--- Returns the second half of an "inOut" or "outIn" easing function.
+-- Returns `nil` for any easing function that isn't in the form of `Ease.inOutXxxx` or `Ease.outInXxxx`. `Ease.linear` returns itself.
+-- @usage
+-- local ease = Ease.outInQuad
+-- local easeExit = Ease.exit(ease)		-- Returns "Ease.inQuad"
 function Ease.exit(__easingFunction)
 	if (componentFunctions[__easingFunction] == nil) then return nil end
 	return componentFunctions[__easingFunction].exit
+end
+
+--- Returns the reverse function of the provided function.
+-- Returns `nil` for any easing function that isn't in the form of `Ease.inXxxx` or `Ease.outXxxx`. `Ease.linear` returns itself.
+-- @usage
+-- local ease = Ease.inQuad
+-- local reverseEase = Ease.reverse(ease) -- Returns "Ease.outQuad"
+function Ease.reverse(__easingFunction)
+	if (reverseFunctions[__easingFunction] == nil) then return nil end
+	return reverseFunctions[__easingFunction]
 end
